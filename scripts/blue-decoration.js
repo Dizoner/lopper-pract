8c90a000d798f443e6cadc86564de704ef4291b7
Grigory <grisha03851@gmail.com>
2022-06-12 17:17:04 +0300

    Update blue-decoration.js

--- scripts/blue-decoration.js
+++ scripts/blue-decoration.js
@@ -1,16 +1,17 @@
-let imgs = document.querySelectorAll('.followus__block img');
+const favicons = document.querySelectorAll('.followus__block img');
+let clicks = 0;
 
-underClass = function () {
-    for (let i = 0; i < imgs.length; i++) {
-        imgs[i].classList.remove('red-decoration');
-        this.classList.add('red-decoration');
-        switch(imgs[i].classList){
-            case 'red-decoration':
-                imgs[i].classList.remove('red-decoration');
-                this.classList.add('blue-decoration');
-    }   }
-};
-
-for (var i = 0; i < imgs.length; i++) {
-    imgs[i].addEventListener('click', underClass);
-}
+favicons.forEach((el) => {
+    el.onclick = () =>{
+        clicks++
+        if(clicks === 1) {
+            el.classList.add("red-decoration");
+        }else if (clicks === 2) {
+            el.classList.replace("red-decoration","blue-decoration");
+        }else if (clicks === 3) {
+            el.classList.remove("red-decoration");
+            el.classList.remove("blue-decoration");
+            clicks = 0;
+        }
+    }
+});
